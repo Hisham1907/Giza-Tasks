4dfcdb0ae030309b86e56f041b3c48698a6694d8
Hisham1907 <Hishamrabea1907@gmail.com>
2024-10-19 19:23:48 +0300

    Fixing icons margin

--- Data Table/content/js/main.js
+++ Data Table/content/js/main.js
@@ -62,14 +62,13 @@ function displayData() {
           <td class="p-3 text-start">${customer.customerCompany}</td>
           <td class="p-3 text-start"><img src="./content/images/${customer.customerImgSrc}" alt="${customer.customerName}"></td>
           <td class="p-3 text-start">${customer.customerCart} €</td>
-          <td class="p-3 text-start d-flex align-content-center">
-            <svg onclick="showData(${idx})" class="me-3 cursor-pointer" data-bs-toggle="modal" data-bs-target="#updateModal" width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <td class="p-3 ">
+            <svg onclick="showData(${idx})" class="me-md-4 mb-2 mb-md-0  cursor-pointer" data-bs-toggle="modal" data-bs-target="#updateModal" width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M3 17.75V21.5H6.75L17.81 10.44L14.06 6.69L3 17.75ZM20.71 7.54C21.1 7.15 21.1 6.52 20.71 6.13L18.37 3.79C17.98 3.4 17.35 3.4 16.96 3.79L15.13 5.62L18.88 9.37L20.71 7.54Z" fill="#357860"/>
             </svg>
             <svg onclick="deleteData(${idx})" class="cursor-pointer"  width="14" height="19" viewBox="0 0 14 19" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M1 16.5C1 17.6 1.9 18.5 3 18.5H11C12.1 18.5 13 17.6 13 16.5V4.5H1V16.5ZM14 1.5H10.5L9.5 0.5H4.5L3.5 1.5H0V3.5H14V1.5Z" fill="#b91c1c"/>
 </svg>
-
             </td>
         </tr>`;
   });
