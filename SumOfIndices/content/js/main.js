5c6ad132af9457db865eb6820a0aae5e10ccb49e
Hisham1907 <Hishamrabea1907@gmail.com>
2024-10-20 11:01:46 +0300

    Adding a user interface with validation and enhancing the task.

--- SumOfIndices/content/js/main.js
+++ SumOfIndices/content/js/main.js
@@ -1,3 +1,8 @@
+"use strict";
+let firstInput = document.querySelector("#arr1"),
+  secondInput = document.querySelector("#arr2"),
+  submitBtn = document.querySelector("#submit-btn");
+
 function sumOfArrays(arr1, arr2) {
   if (arr1.length > arr2.length) {
     let plus = arr1.length - arr2.length;
@@ -15,5 +20,47 @@ function sumOfArrays(arr1, arr2) {
     return arr3;
   }
 }
-let result = sumOfArrays([1, 0, 2, 3, 4], [3, 5, 6, 7, 8, 13]);
-console.log(result);
+function clearInputs() {
+  firstInput.value = "";
+  secondInput.value = "";
+}
+function isValidNumberArray(input) {
+  return /^\s*\[\s*(-?\d+(\.\d+)?\s*,\s*)*-?\d+(\.\d+)?\s*\]\s*$/.test(input);
+  // I didn't make this regex to be honest 😀, I searched for it .
+}
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (firstInput.value.trim() === "" || secondInput.value.trim() === "") {
+    Swal.fire({
+      title: "Wake Up!",
+      text: `Input fields can't be empty`,
+      icon: "error",
+      confirmButtonText: "OK",
+    });
+  } 
+  else if (
+    !isValidNumberArray(firstInput.value) ||
+    !isValidNumberArray(secondInput.value)
+  ) {
+    Swal.fire({
+      title: "Invalid Input!",
+      text: `Please enter valid arrays of numbers in the format [1, 2, 3].`,
+      icon: "error",
+      confirmButtonText: "OK",
+    });
+  } else {
+    let arr1 = JSON.parse(firstInput.value);
+    let arr2 = JSON.parse(secondInput.value);
+    let result = sumOfArrays(arr1, arr2);
+    Swal.fire({
+      title: "✨ Array Summed Successfully!",
+      text: `Result: [${result}]`,
+      icon: "success",
+      confirmButtonText: "Awesome!",
+    });
+
+    clearInputs();
+  }
+});
